56c295f15ad02b442f3636ce10bae82ea3bdb286
sachin <sachin3072004@gmail.com>
2026-02-13 13:34:53 +0000

    REmoved xsk_ping_pong

--- ebpf/examples/xdp/xdp-switch/hello.bpf.c
+++ ebpf/examples/xdp/xdp-switch/hello.bpf.c
@@ -19,6 +19,13 @@ struct {
 	__type(value, struct Value);
 } hmap SEC(".maps");
 
+struct {
+        __uint(type, BPF_MAP_TYPE_XSKMAP);
+        __type(key, __u32);
+        __type(value, __u32);
+        __uint(max_entries, 64);
+} src1  SEC(".maps");
+
 SEC("xdp")
 int capture_pkt(struct xdp_md* ctx){
 	void *data = (void *)(long)ctx->data;
@@ -38,6 +45,17 @@ int capture_pkt(struct xdp_md* ctx){
 		bpf_map_update_elem(&hmap,&k,&init, BPF_ANY);
 	}else{
 		__sync_fetch_and_add(&v->count, 1);
+	}
+	if(proto == 2048){
+		int index = ctx->rx_queue_index;
+		bpf_printk("Index == %d\n",index);
+		 if (bpf_map_lookup_elem(&src1, &index)){
+			 bpf_printk(" Socket hai\n");
+                	return bpf_redirect_map(&src1, index, 0);
+		 }else{
+			 bpf_printk("Sorry No Socket\n");
+		 }
+
 	}
 	return XDP_PASS;
 }
